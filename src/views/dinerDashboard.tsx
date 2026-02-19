5cbb3fc64d21996ba4b5143cde40a7e4172bacd2
Jordan Brockbank <132620463+JBrockbank@users.noreply.github.com>
2026-02-18 20:04:06 -0700

    Added backend persistence to update user

--- src/views/dinerDashboard.tsx
+++ src/views/dinerDashboard.tsx
@@ -26,19 +26,21 @@ export default function DinerDashboard(props: Props) {
   }, [user]);
 
   async function updateUser() {
-  let updatedUser: User = {
-    id: user.id,
-    name: nameRef.current?.value,
-    email: emailRef.current?.value,
-    password: passwordRef.current?.value || undefined,
-    roles: user.roles,
-  };
+    let updatedUser: User = {
+      id: user.id,
+      name: nameRef.current?.value,
+      email: emailRef.current?.value,
+      password: passwordRef.current?.value || undefined,
+      roles: user.roles,
+    };
 
-  props.setUser(updatedUser);
-  setTimeout(() => {
-    HSOverlay.close(document.getElementById('hs-jwt-modal')!);
-  }, 100);
-}
+    await pizzaService.updateUser(updatedUser);
+
+    props.setUser(updatedUser);
+    setTimeout(() => {
+      HSOverlay.close(document.getElementById("hs-jwt-modal")!);
+    }, 100);
+  }
 
   const nameRef = React.useRef<HTMLInputElement>(null);
   const emailRef = React.useRef<HTMLInputElement>(null);
